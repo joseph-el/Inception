b6afe468ecf244f28205f9bccf6a8c87ce0548b2
Youssef El Idrissi <yoel-idr@c3r6p1.1337.ma>
2023-08-18 18:23:01 +0100

    sd

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -2,9 +2,16 @@
 
 mkdir -p /run/openrc ; touch /run/openrc/softlevel ; openrc &
 
+sleep 5
+
 echo 'rc_provide="loopback net"' >> /etc/rc.conf
 
 /etc/init.d/php-fpm81 start
+
+wp core install --url=$URL_NAME --title=$SB_USER --admin_user=$ADMIN_USER --admin_password=$DB_PASS --admin_email=$USER_EMAIL
+wp user create  lina ilyas@gmail.com --role=subscriber
+wp plugin install redis-cache --activate
+
 
 /etc/init.d/php-fpm81 stop
 
